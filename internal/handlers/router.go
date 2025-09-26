6ed5571a83e835315db9fea35baf304a9dc1d902
Vladimirmoscow84 <ccr1@yandex.ru>
2025-09-26 14:46:35 +0300

    addnoticegandler done

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/Vladimirmoscow84/Delayed_Notifier.git/internal/model"
 	"github.com/Vladimirmoscow84/Delayed_Notifier.git/internal/storage"
 	"github.com/gin-gonic/gin"
 	"github.com/wb-go/wbf/ginext"
@@ -27,9 +29,23 @@ func (r *Router) Routers() {
 }
 
 func (r *Router) addNotice(c *gin.Context) {
-	c.JSON(http.StatusBadRequest, gin.H{"error": "order UID is required"})
+	ctx := c.Request.Context()
 
-	notice := 0
-	c.
-		r.store.AddNotice()
+	var notice model.Notice
+
+	if err := c.ShouldBindJSON(&notice); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invlid input data."})
+		return
+	}
+
+	notice.DateCreated = time.Now()
+	notice.SendAttempts = 5
+	notice.SendStatus = "sheduled"
+
+	id, err := r.store.AddNotice(ctx, notice)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": id})
 }
--- internal/model/model.go
+++ internal/model/model.go
@@ -4,9 +4,9 @@ import "time"
 
 type Notice struct {
 	Id           int       `json:"id"`
-	Body         string    `json:"body"`
+	Body         string    `json:"body" binding:"required"`
 	DateCreated  time.Time `json:"date_created"`
-	SendDate     time.Time `json:"send_date"`
+	SendDate     time.Time `json:"send_date" binding:"required"`
 	SendAttempts int       `json:"send_attempts"`
-	SendStatus   bool      `json:"send_status"`
+	SendStatus   string    `json:"send_status"`
 }
