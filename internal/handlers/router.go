5084ed09fbe4e1ff0edb4a3c1b6b11de43ecc4f7
Vladimirmoscow84 <ccr1@yandex.ru>
2025-09-28 22:56:36 +0300

    addNotice add. single file

--- internal/handlers/router.go
+++ internal/handlers/router.go
@@ -1,12 +1,7 @@
 package handlers
 
 import (
-	"net/http"
-	"time"
-
-	"github.com/Vladimirmoscow84/Delayed_Notifier.git/internal/model"
 	"github.com/Vladimirmoscow84/Delayed_Notifier.git/internal/storage"
-	"github.com/gin-gonic/gin"
 	"github.com/wb-go/wbf/ginext"
 )
 
@@ -26,26 +21,4 @@ func (r *Router) Routers() {
 	r.Router.POST("/notify", r.addNotice)
 	//r.Router.Get("/notify/:id", r.getStatus)
 	//r.Router.Delete("/notify/:id", r.deleteNotice)
-}
-
-func (r *Router) addNotice(c *gin.Context) {
-	ctx := c.Request.Context()
-
-	var notice model.Notice
-
-	if err := c.ShouldBindJSON(&notice); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invlid input data."})
-		return
-	}
-
-	notice.DateCreated = time.Now()
-	notice.SendAttempts = 5
-	notice.SendStatus = "sheduled"
-
-	id, err := r.store.AddNotice(ctx, notice)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"id": id})
 }
