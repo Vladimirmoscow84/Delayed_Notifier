8420c325fea5bfd9db8ae3c9f106ae04e1ed6595
Vladimirmoscow84 <ccr1@yandex.ru>
2025-09-28 23:47:27 +0300

    deleteNotice handler done

---
+++ internal/handlers/deleteNotice.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (r *Router) deleteNotice(c *gin.Context) {
+	ctx := c.Request.Context()
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil || id <= 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid or missing ID parameter of notice"})
+		return
+	}
+
+	err = r.store.DeleteNotice(ctx, id)
+	if err != nil {
+		log.Printf("error deleting notice by ID=%d: %v\n", id, err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "an internal error while deleting the notice by ID"})
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
