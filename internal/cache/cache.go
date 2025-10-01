e1a5ba88927116d87fc68fc0482c80cdfb445fb8
Vladimirmoscow84 <ccr1@yandex.ru>
2025-10-01 22:06:21 +0300

    cache probably done!

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"context"
 	"time"
 
 	"github.com/wb-go/wbf/redis"
@@ -18,4 +19,14 @@ func NewCache(client *redis.Client) *Cache {
 		client:   client,
 		strategy: retry.Strategy{Attempts: 5, Delay: 3 * time.Second, Backoff: 3},
 	}
+}
+
+// Get - получение значения из кэш
+func (c *Cache) Get(ctx context.Context, strategy retry.Strategy, key string) (string, error) {
+	return c.client.GetWithRetry(ctx, c.strategy, key)
+}
+
+// Set - установление значения в кэш
+func (c *Cache) Set(ctx context.Context, strategy retry.Strategy, key string, value any) error {
+	return c.client.SetWithRetry(ctx, c.strategy, key, value)
 }
