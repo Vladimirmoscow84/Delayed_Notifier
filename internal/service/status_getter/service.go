ec91ebfb519bdbfe463179ab751cc07b18eb23be
Vladimirmoscow84 <ccr1@yandex.ru>
2025-10-16 09:58:40 +0300

    fix statusGettre with strategy

--- internal/service/status_getter/service.go
+++ internal/service/status_getter/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/wb-go/wbf/retry"
 )
@@ -23,19 +24,40 @@ func New(c cache) *StatusGetter {
 func (s *StatusGetter) GetStatusNotice(ctx context.Context, key string) (string, error) {
 	str := retry.Strategy{
 		Attempts: 5,
+		Delay:    300 * time.Millisecond,
+	}
+	var exists bool
+	var value string
+	var err error
+	//проверка наличия ключа в соответстви со стратегией попыток
+	for i := 0; i < str.Attempts; i++ {
+		exists, err = s.cache.Exists(ctx, key)
+		if err == nil {
+			break
+		}
+		log.Printf("[status_getter] attempt %d to check key from cache failed: %v", i)
+		time.Sleep(str.Delay)
 	}
 
-	exists, err := s.cache.Exists(ctx, key)
 	if err != nil {
-		return "", fmt.Errorf("[status_getter] failed to check cache key: %w", err)
+		return "", fmt.Errorf("[status_getter] failed to check cache key after %d attempts: %w", str.Attempts, err)
 	}
 	if !exists {
 		return "", fmt.Errorf("[status_getter] key %q not found in cache", key)
 	}
 
-	value, err := s.cache.Get(ctx, str, key)
+	//получение значений из кэш по ключу в соответствии со стратегией
+	for i := 0; i < str.Attempts; i++ {
+		value, err = s.cache.Get(ctx, str, key)
+		if err == nil {
+			break
+		}
+		log.Printf("[status_getter] attempt %d to get key from cache failed: %v", i+1, err)
+		time.Sleep(str.Delay)
+	}
+
 	if err != nil {
-		return "", fmt.Errorf("[status_getter] failed to get notice from cache: %w", err)
+		return "", fmt.Errorf("[status_getter] failed to get notice from cache afte %d attempts: %w", str.Attempts, err)
 	}
 
 	log.Printf("[status_getter] got cached status for key %q: %v", key, value)
