cdf9e0c4c9118bae69107d03e1d2c9a568393f75
Vladimirmoscow84 <ccr1@yandex.ru>
2025-10-16 17:02:16 +0300

    telega fix

--- internal/notifier/telegram/telegram.go
+++ internal/notifier/telegram/telegram.go
@@ -1,6 +1,7 @@
 package telegram
 
 import (
+	"errors"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -12,14 +13,19 @@ type Client struct {
 }
 
 func New(token string, chatID int64) (*Client, error) {
-	if token == "" || chatID == 0 {
-		log.Println("[telega]TELEGRAM_TIKEN or TELEGRAM_CHAT_ID is not specified, no messages will be sent in telegram")
+	if token == "" {
+		log.Println("[telega]TELEGRAM_TOKEN is not specified, no messages will be sent in telegram")
+		return nil, errors.New("[telega]TELEGRAM_TOKEN is not specified")
+	}
+	if chatID == 0 {
+		log.Println("[telega]TELEGRAM_CHAT_ID is not specified, no messages will be sent in telegram")
+		return nil, errors.New("[telega]TELEGRAM_CHAT_ID is not specified")
 	}
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		return nil, err
 	}
-	log.Println("[telega] Client successfully created")
+	log.Printf("[telega] Client successfully created. Authorized on account %s", bot.Self.UserName)
 	return &Client{
 		chatID: chatID,
 		bot:    bot,
@@ -28,11 +34,15 @@ func New(token string, chatID int64) (*Client, error) {
 
 // Send - отправляет уведомление в телеграм
 func (c *Client) Send(message string) error {
+	if c.bot == nil {
+		return errors.New("[telega] bot client is nil")
+	}
 	msg := tgbotapi.NewMessage(c.chatID, message)
+	msg.DisableWebPagePreview = true
 	_, err := c.bot.Send(msg)
 	if err != nil {
 		return err
 	}
-	log.Println("[telega] message successfully sended")
+	log.Printf("[telega] message successfully sent to chatID %d", c.chatID)
 	return nil
 }
